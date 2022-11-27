fb81052950d09890b74c39dc7bdff42f1fe0776d
Piotreq006 <piotrolszewski618@gmail.com>
2022-11-27 16:47:08 +0100

    Rysowanie, kolory,  gumka

--- PaintApp/PaintApp/MainWindow.xaml.cs
+++ PaintApp/PaintApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,119 @@ namespace PaintApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        Button btnPen = new Button();
+        Button btnEraser = new Button();
+        Button btnRed = new Button();
+        Button btnGreen = new Button();
+        Button btnBlue = new Button();
         public MainWindow()
         {
             InitializeComponent();
+            this.Title = "Paint After 1 year in Russia";
+            this.WindowState = System.Windows.WindowState.Maximized;
+            SolidColorBrush brsh = new SolidColorBrush(Colors.Gray);
+            MyGrid.Background = (Brush)brsh;
+            MyCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+            MyCanvas.DefaultDrawingAttributes.Width = 50;
+            MyCanvas.DefaultDrawingAttributes.Height = 50;
+
+            MyCanvas.Height = System.Windows.SystemParameters.WorkArea.Height - 150;
+            MyCanvas.Width = System.Windows.SystemParameters.WorkArea.Width;
+            MyCanvas.Margin = new Thickness(0, 0, 0, 0);
+            MyCanvas.VerticalAlignment = VerticalAlignment.Bottom;
+            MyCanvas.HorizontalAlignment = HorizontalAlignment.Left;
+
+            MyCanvas.UseCustomCursor = true;
+            AddButtonOnGreed();
+        }
+
+        public void AddButtonOnGreed()
+        {
+            btnPen.Content = "Pen";
+            btnPen.Name = "btnPen";
+            btnPen.HorizontalAlignment = HorizontalAlignment.Left;
+            btnPen.VerticalAlignment = VerticalAlignment.Top;
+            btnPen.Width = 100;
+            btnPen.Height = 50;
+            btnPen.Margin = new Thickness(80, 40, 0, 0);
+            btnPen.Click += new RoutedEventHandler(btnClick);
+            MyGrid.Children.Add(btnPen);
+
+            btnEraser.Content = "Eraser";
+            btnEraser.Name = "btnEraser";
+            btnEraser.HorizontalAlignment = HorizontalAlignment.Left;
+            btnEraser.VerticalAlignment = VerticalAlignment.Top;
+            btnEraser.Width = 100;
+            btnEraser.Height = 50;
+            btnEraser.Margin = new Thickness(200, 40, 0, 0);
+            btnEraser.Click += new RoutedEventHandler(btnClick);
+            MyGrid.Children.Add(btnEraser);
+
+            btnRed.Background = new SolidColorBrush(Colors.Red);
+            btnRed.Name = "btnRed";
+            btnRed.HorizontalAlignment = HorizontalAlignment.Left;
+            btnRed.VerticalAlignment = VerticalAlignment.Top;
+            btnRed.Width = 20;
+            btnRed.Height = 20;
+            btnRed.Margin = new Thickness(90, 100, 0, 0);
+            btnRed.Click += new RoutedEventHandler(btnClick);
+            MyGrid.Children.Add(btnRed);
+
+            btnGreen.Background = new SolidColorBrush(Colors.Green);
+            btnGreen.Name = "btnGreen";
+            btnGreen.HorizontalAlignment = HorizontalAlignment.Left;
+            btnGreen.VerticalAlignment = VerticalAlignment.Top;
+            btnGreen.Width = 20;
+            btnGreen.Height = 20;
+            btnGreen.Margin = new Thickness(120, 100, 0, 0);
+            btnGreen.Click += new RoutedEventHandler(btnClick);
+            MyGrid.Children.Add(btnGreen);
+
+            btnBlue.Background = new SolidColorBrush(Colors.Blue);
+            btnBlue.Name = "btnBlue";
+            btnBlue.HorizontalAlignment = HorizontalAlignment.Left;
+            btnBlue.VerticalAlignment = VerticalAlignment.Top;
+            btnBlue.Width = 20;
+            btnBlue.Height = 20;
+            btnBlue.Margin = new Thickness(150, 100, 0, 0);
+            btnBlue.Click += new RoutedEventHandler(btnClick);
+            MyGrid.Children.Add(btnBlue);
+        }
+
+        public void btnClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            switch (btn.Name)
+            {
+                case "btnPen":
+                    {
+                        MyCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                        MyCanvas.Cursor = System.Windows.Input.Cursors.Pen;
+                    } break;
+
+                case "btnEraser":
+                    {
+                        MyCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                        MyCanvas.Cursor = System.Windows.Input.Cursors.Cross;
+                    } break;
+
+                case "btnRed":
+                    {
+                        MyCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+                    } break;
+
+                case "btnGreen":
+                    {
+                        MyCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+                    }
+                    break;
+
+                case "btnBlue":
+                    {
+                        MyCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+                    }
+                    break;
+            }
         }
     }
 }
