8714d2605405ddb6c96549749ecf10ee22c0d859
macholix <bartosz.machola@vp.pl>
2022-11-28 20:38:22 +0100

    Zapisywanie i wczytywanie

--- PaintApp/PaintApp/MainWindow.xaml.cs
+++ PaintApp/PaintApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -8,7 +10,9 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -26,6 +30,8 @@ namespace PaintApp
         Button btnRed = new Button();
         Button btnGreen = new Button();
         Button btnBlue = new Button();
+        Button btnSave = new Button();
+        Button btnLoad = new Button();
         public MainWindow()
         {
             InitializeComponent();
@@ -53,7 +59,7 @@ namespace PaintApp
             btnPen.Name = "btnPen";
             btnPen.HorizontalAlignment = HorizontalAlignment.Left;
             btnPen.VerticalAlignment = VerticalAlignment.Top;
-            btnPen.Width = 100;
+            btnPen.Width = 80;
             btnPen.Height = 50;
             btnPen.Margin = new Thickness(80, 40, 0, 0);
             btnPen.Click += new RoutedEventHandler(btnClick);
@@ -63,10 +69,10 @@ namespace PaintApp
             btnEraser.Name = "btnEraser";
             btnEraser.HorizontalAlignment = HorizontalAlignment.Left;
             btnEraser.VerticalAlignment = VerticalAlignment.Top;
-            btnEraser.Width = 100;
+            btnEraser.Width = 80;
             btnEraser.Height = 50;
-            btnEraser.Margin = new Thickness(200, 40, 0, 0);
-            btnEraser.Click += new RoutedEventHandler(btnClick);
+            btnEraser.Margin = new Thickness(150, 40, 0, 0);
+            btnEraser.Click += new RoutedEventHandler(btnClick); 
             MyGrid.Children.Add(btnEraser);
 
             btnRed.Background = new SolidColorBrush(Colors.Red);
@@ -98,6 +104,26 @@ namespace PaintApp
             btnBlue.Margin = new Thickness(150, 100, 0, 0);
             btnBlue.Click += new RoutedEventHandler(btnClick);
             MyGrid.Children.Add(btnBlue);
+
+            btnSave.Content = "Save";
+            btnSave.Name = "btnSave";
+            btnSave.HorizontalAlignment = HorizontalAlignment.Left;
+            btnSave.VerticalAlignment = VerticalAlignment.Top;
+            btnSave.Width = 80;
+            btnSave.Height = 50;
+            btnSave.Margin = new Thickness(220, 40, 0, 0);
+            btnSave.Click += new RoutedEventHandler(btnClick);
+            MyGrid.Children.Add(btnSave);
+
+            btnLoad.Content = "Load";
+            btnLoad.Name = "btnLoad";
+            btnLoad.HorizontalAlignment = HorizontalAlignment.Left;
+            btnLoad.VerticalAlignment = VerticalAlignment.Top;
+            btnLoad.Width = 80;
+            btnLoad.Height = 50;
+            btnLoad.Margin = new Thickness(290, 40, 0, 0);
+            btnLoad.Click += new RoutedEventHandler(btnClick);
+            MyGrid.Children.Add(btnLoad);
         }
 
         public void btnClick(object sender, RoutedEventArgs e)
@@ -109,18 +135,21 @@ namespace PaintApp
                     {
                         MyCanvas.EditingMode = InkCanvasEditingMode.Ink;
                         MyCanvas.Cursor = System.Windows.Input.Cursors.Pen;
-                    } break;
+                    }
+                    break;
 
                 case "btnEraser":
                     {
                         MyCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
                         MyCanvas.Cursor = System.Windows.Input.Cursors.Cross;
-                    } break;
+                    }
+                    break;
 
                 case "btnRed":
                     {
                         MyCanvas.DefaultDrawingAttributes.Color = Colors.Red;
-                    } break;
+                    }
+                    break;
 
                 case "btnGreen":
                     {
@@ -133,7 +162,36 @@ namespace PaintApp
                         MyCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
                     }
                     break;
+                case "btnSave":
+                    {
+                        SaveFileDialog saveFileDialog12 = new SaveFileDialog();
+                        saveFileDialog12.Filter = "InkCanvas Format|*.sandy";
+                        saveFileDialog12.Title = "Save InkCanvas File";
+                        saveFileDialog12.ShowDialog();
+                        if (saveFileDialog12.FileName == "") return;
+                        FileStream fs = File.Open(saveFileDialog12.FileName, FileMode.OpenOrCreate);
+                        MyCanvas.Strokes.Save(fs);
+                        fs.Close();
+                    }
+                    break;
+                case "btnLoad":
+                    {
+                        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                        openFileDialog1.Title = "Load InkCanvas File";
+                        openFileDialog1.DefaultExt = "sandy";
+                        openFileDialog1.Filter = "Inkcanvas Format(.sandy)|*.sandy";
+                        openFileDialog1.ShowDialog();
+                        if (openFileDialog1.FileName == "") return;
+                        FileStream fs = File.Open(openFileDialog1.FileName, FileMode.Open);
+                        StrokeCollection myStrk = new StrokeCollection(fs);
+                        MyCanvas.Strokes = myStrk;
+                        fs.Close();
+
+                    }
+                    break;
             }
         }
     }
+
+
 }
